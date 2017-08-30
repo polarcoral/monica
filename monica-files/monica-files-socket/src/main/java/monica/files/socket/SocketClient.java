ecb88c374a6780f3d8656418755d6abf5e45e212
sliroo <carolin@polarcoral.com>
2017-08-30 15:25:53 +0800

    Remove unused code in SocketClient.java

--- monica-files/monica-files-socket/src/main/java/monica/files/socket/SocketClient.java
+++ monica-files/monica-files-socket/src/main/java/monica/files/socket/SocketClient.java
@@ -46,18 +46,6 @@ public class SocketClient implements Client {
 
 			// Read commands from the stdin.
 			ChannelFuture lastWriteFuture = null;
-			/*
-			 * BufferedReader in = new BufferedReader(new
-			 * InputStreamReader(System.in)); for (;;) { String line =
-			 * in.readLine(); if (line == null) { break; }
-			 * 
-			 * // Sends the received line to the server. lastWriteFuture =
-			 * ch.writeAndFlush(line + "\r\n");
-			 * 
-			 * // If user typed the 'bye' command, wait until the server closes
-			 * // the connection. if ("bye".equals(line.toLowerCase())) {
-			 * ch.closeFuture().sync(); break; } }
-			 */
 
 			File file = new File("F:\\aa.txt");
 			RandomAccessFile raf = null;
