63f5485e99de3ea35227a8a159015c1540f45d2b
WreathCrystal <24571465@qq.com>
2019-12-21 20:40:54 +0800

    Add disk storage example

---
+++ monica-examples/src/main/java/monica/examples/storage/DiskStorageFactory.java
@@ -0,0 +1,17 @@
+package monica.examples.storage;
+
+import monica.framework.Storage;
+import monica.framework.storage.AbstractStorageFactory;
+
+public class DiskStorageFactory extends AbstractStorageFactory{
+	private static Storage diskStorage = new StorageOnDisk();
+
+	@Override
+	public  Storage newStorageInstance() {
+		if(null == diskStorage){
+			diskStorage = new StorageOnDisk();
+		}
+		return diskStorage;
+	}
+
+}
